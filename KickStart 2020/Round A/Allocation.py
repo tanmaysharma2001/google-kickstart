bcf17f3a64c358fcd55d2ae7028bb0fe96674f3c
Tanmay Sharma <tsharma_1@edu.hse.ru>
2021-02-27 20:17:41 +0530

    Add comments to Allocation.py

--- KickStart 2020/Round A/Allocation.py
+++ KickStart 2020/Round A/Allocation.py
@@ -1,14 +1,18 @@
 t = int(input())
+# Number of test cases!
 
 for t_itr in range(1, t + 1):
 
     integers = input().split()
 
-    n = int(integers[0])
+    n = int(integers[0])      # Value of N
 
-    b = int(integers[1])
+    b = int(integers[1])      # Value of B
 
     prices = list(map(int, input().split()))
+    # All the prices which need to be sorted
+    # so that we can easily retrieve the required
+    # elements.
 
     prices = sorted(prices)
 
